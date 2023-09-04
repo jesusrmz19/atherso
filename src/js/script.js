b07d1dbacd0d4f869c3b6986dee04b516dfe3083
Jesus Ramirez <jrmz.maldonado@gmail.com>
2023-09-04 14:31:08 -0400

    Add CSS for smaller images

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,7 @@
 console.log('1, 2, 3... Testing Parcel 📦');
+const navBtn = document.querySelector(".nav--btn");
+const nav = document.querySelector(".nav--list");
+
+navBtn.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
